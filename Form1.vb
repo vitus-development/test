4a39657b9c3a9120bd3959456f31e7175e0bda54
Vitus-boop <53561541+Vitus-boop@users.noreply.github.com>
2019-08-01 12:54:04 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Inteiro_Financial_InsightDataSet.FinansialInsightLogin' table. You can move, or remove it, as needed.
+        Me.FinansialInsightLoginTableAdapter.Fill(Me.Inteiro_Financial_InsightDataSet.FinansialInsightLogin)
+        'TODO: This line of code loads data into the 'BrugerDatasæt.Bruger' table. You can move, or remove it, as needed.
+        Me.BrugerTableAdapter.Fill(Me.BrugerDatasæt.Bruger)
+
+    End Sub
+End Class
